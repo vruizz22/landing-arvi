7290609eb5d4e245e988a37e7081957ab0b6e508
Victor Ruiz <veruiz@uc.cl>
2026-03-25 16:38:31 -0300

    refactor(format/*): Se ejecuta pnpm format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arvi.cl',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [
-    icon(),
-    sitemap()
-  ]
-});
+  integrations: [icon(), sitemap()],
+})
--- eslint.config.js
+++ eslint.config.js
@@ -4,30 +4,30 @@ import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
 
 export default [
-    eslint.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettier, // Esto debe estar al final para anular cualquier regla que entre en conflicto con Prettier
-    {
-        ignores: ['dist', 'node_modules'],
+  eslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier, // Esto debe estar al final para anular cualquier regla que entre en conflicto con Prettier
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.astro'],
+    parser: 'astro-eslint-parser',
+    parserOptions: {
+      parser: '@typescript-eslint/parser',
+      extraFileExtensions: ['.astro'],
     },
-    {
-        files: ['**/*.astro'],
-        parser: 'astro-eslint-parser',
-        parserOptions: {
-            parser: '@typescript-eslint/parser',
-            extraFileExtensions: ['.astro'],
-        },
-        rules: {
-            // reglas específicas para archivos Astro
-            'comma-dangle': ['error', 'always-multiline'],
-        },
+    rules: {
+      // reglas específicas para archivos Astro
+      'comma-dangle': ['error', 'always-multiline'],
     },
-    {
-        files: ['**/*.{ts,tsx,js,jsx}'],
-        parser: '@typescript-eslint/parser',
-        rules: {
-            'comma-dangle': ['error', 'always-multiline'],
-        },
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    parser: '@typescript-eslint/parser',
+    rules: {
+      'comma-dangle': ['error', 'always-multiline'],
     },
-];
+  },
+]
